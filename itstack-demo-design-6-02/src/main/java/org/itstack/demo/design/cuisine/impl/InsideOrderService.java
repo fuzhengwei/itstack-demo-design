cd20a705b3d1400855ca8b39d5050e392bee1cb5
fuzhengwei <184172133@qq.com>
2020-06-21 17:24:07 +0800

    小傅哥 | 重学 Java 设计模式：实战适配器模式「从多个MQ消息体中，抽取指定字段值场景」

--- itstack-demo-design-6-02/src/main/java/org/itstack/demo/design/cuisine/impl/InsideOrderService.java
+++ itstack-demo-design-6-02/src/main/java/org/itstack/demo/design/cuisine/impl/InsideOrderService.java
@@ -1,4 +1,4 @@
-package org.itstack.demo.design.impl;
+package org.itstack.demo.design.cuisine.impl;
 
 import org.itstack.demo.design.OrderAdapterService;
 import org.itstack.demo.design.service.OrderService;
